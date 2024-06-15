cde6acf24905a08419473c4e8ee92ff1e31e43d7
Ayan Yadav <ayan24092003@gmail.com>
2024-06-15 16:24:22 +0530

    made required components and did necessary changes to App.jsx

--- Context-API/todo-App/src/App.jsx
+++ Context-API/todo-App/src/App.jsx
@@ -1,5 +1,7 @@
 import { TodoProvider } from './contexts'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -12,9 +14,26 @@ function App() {
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((prevTodo) => prevTodo.id != id))
   }
-  const toggleComplete = (id) =>{
-    setTodos()
+  const toggleComplete = (id) => {
+    setTodos((prev) => 
+      prev.map((prevTodo) => 
+        prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo
+      )
+    )
   }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem('todos'))
+
+    if(todos && todos.length > 0){
+      setTodos((todos))
+    }
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  },[todos])
+  
 
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
